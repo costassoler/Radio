aec99e440f1c66d41d6073cba632138293a47faf
Sean Stroud <srstroud@ltsp191.ugastro.berkeley.edu>
2019-02-13 17:55:32 -0800

    Imporved spectra.py module

---
+++ lab2/spectra.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def voltage_spectra(data, volt):
+    converted = data*volt/2**15
+    converted=converted[0,16000]+converted[16001:]*1j
+    voltage = np.fft.fft(converted)
+    freqs = np.fft.fftfreq(len(converted))
+
+    return freqs, voltage
+
+
+def power_spectra(data, volt):
+    freqs, voltage = voltage_spectra(data, volt)
+    power = np.abs(voltage)**2
+
+    return freqs, power
+
+
+def plot_power(data, volt, name):
+    freqs, power = power_spectra(data, volt)
+    powershift = np.fft.fftshift(power)
+    freqsshift = np.fft.fftshift(freqs)
+
+    plt.plot(freqsshift, powershift)
+    plt.title("Power Spectrum of " + name)
+    plt.ylabel("Power")
+    plt.xlabel("Frequency")
+    plt.show()
