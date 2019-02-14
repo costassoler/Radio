b4357a515a653d8589f914c42d05e435e1274798
Missa233 <missacarlson@berkeley.edu>
2019-02-14 13:59:04 -0800

    Add files via upload

--- collectdata.py
+++ collectdata.py
@@ -1,20 +1,14 @@
 import ugradio
 import numpy as np
-import matplotlib.pyplot as plt
+import matplitlib.pyplot as plt
 
 
-def collect1st(volt_range):
+def collect(volt_range, blocks, numrepeat, name):
     """volt_range: str """
-    first_data = ugradio.pico.capture_data(volt_range, divisor=10, dual_mode=True)
-    np.savetxt('firsthorn', first_data)
 
-    plt.hist(first_data)
-    plt.title('Histogramed First Sample')
-    plt.ylabel("Counts")
-    plt.xlabel("Pico Value")
-    plt.savefig('firsthist.pdf')
-
-    return np.loadtxt("firsthorn")
+    for i in range (0,numrepeat):
+        data = ugradio.pico.capture_data(volt_range, divisor=10, dual_mode=True, nblocks=blocks)
+        np.savetxt(name+i,data)
 
 def insert_upper(volt_range):
     data = ugradio.pico.capture_data(volt_range, divisor=10, dual_mode=True)
