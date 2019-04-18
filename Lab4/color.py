41244e3e2c4fc521e4d6d19113d5047261cc236a
Sean Stroud <39713275+srstroud@users.noreply.github.com>
2019-04-17 17:31:22 -0700

    Added color function for range of pointing

---
+++ Lab4/color.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def color(alt, az):
+
+    """
+    Inputs altitude and azimuth arrays and determines which points lie in range for Leuschner.
+    Arguments:
+    alt, altitude (array-like)
+    az, azimuth (array-like)
+    Returns:
+    col, color code for plotting (array-like)
+    ind, indicies of out of range points (array-like)
+    """
+
+    flalt = alt.flatten()
+    flaz = az.flatten()
+
+    min_alt, max_alt = 15, 85
+    min_azi, max_azi = 5, 350
+    
+    col = ['b']*len(flalt)
+    col = np.array(col)
+    col[np.where(flalt<min_alt)] = 'r'
+    col[np.where(flaz<min_azi)] = 'r'
+    col[np.where(flalt>max_alt)] = 'r'
+    col[np.where(flaz>max_azi)] = 'r'
+    
+    ind = np.where(col == 'r')
+   
+    return col, ind
