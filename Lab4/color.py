cf802189abc1d9e5bac05cfeceaf3f270e0d3cf1
Sean Stroud <39713275+srstroud@users.noreply.github.com>
2019-04-18 16:29:05 -0700

    Update color.py

--- Lab4/color.py
+++ Lab4/color.py
@@ -1,30 +1,30 @@
 import numpy as np
 
 def color(alt, az):
-
     """
     Inputs altitude and azimuth arrays and determines which points lie in range for Leuschner.
     Arguments:
-    alt, altitude (array)
-    az, azimuth (array)
+    alt, altitude (array-like)
+    az, azimuth (array-like)
     Returns:
     col, color code for plotting (array-like)
-    ind, indicies of out of range points (array-like)
+    ind_bad, indicies of out of range points (array-like)
+    ind_good, indicies of in range points (array-like)
     """
-
     flalt = alt.flatten()
     flaz = az.flatten()
 
-    min_alt, max_alt = 15, 85  #Obtained from ugradio.leusch
-    min_az, max_az = 5, 350    #Obtained from ugradio.leusch
+    min_alt, max_alt = 15, 85
+    min_azi, max_azi = 5, 350
     
-    col = ['b']*len(flalt)
+    col = ['g']*len(flalt)
     col = np.array(col)
     col[np.where(flalt<min_alt)] = 'r'
-    col[np.where(flaz<min_az)] = 'r'
+    col[np.where(flaz<min_azi)] = 'r'
     col[np.where(flalt>max_alt)] = 'r'
-    col[np.where(flaz>max_az)] = 'r'
+    col[np.where(flaz>max_azi)] = 'r'
     
-    ind = np.where(col == 'r')
+    ind_bad = np.where(col == 'r')
+    ind_good = np.where(col == 'g')
    
-    return col, ind
+    return col, ind_bad, ind_good
